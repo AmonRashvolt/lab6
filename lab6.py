e6ec1f68af3797c44871eae33623a53ecfc55288
AmonRashvolt <62507363+AmonRashvolt@users.noreply.github.com>
2020-04-27 15:56:14 +0300

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -1,8 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-fig2=plt.figure()
-ax1=plt.subplot('121')
+fig, ax = plt.subplots()
+
+
 data = np.loadtxt("C:\\Users\\serge\\source\\repos\\lab6\\ans1.txt")
 plt.grid()
-ax1.plot(data[:,0],data[:,1],marker='o')
+ax.plot(data[:,0],data[:,1],marker='o')
+Z = np.exp(-data[:,0]**2 - data[:,1]**2)
+CS = ax.contour(data[:,0],data[:,1], Z)
+ax.clabel(CS, inline=1, fontsize=10)
 plt.show()
